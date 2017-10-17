c6772929053eb986e413e2767cfada2d749ed63f
songzxDev <songzx_2326@163.com>
2017-10-17 21:46:55 +0800

    Exchanger类的exchanger()方法的阻塞特性和传递数据功能

---
+++ src/cn/songzx/semaph/eighth/extthread/MyEighthThreadB.java
@@ -0,0 +1,48 @@
+/**   
+* @Title: MyEighthThreadA.java 
+* @Package cn.songzx.semaph.eighth.extthread 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author Songzx songzx_2326@163.com   
+* @date 2017年10月17日 下午9:34:48 
+* @version V1.0   
+*/
+package cn.songzx.semaph.eighth.extthread;
+
+import java.util.concurrent.Exchanger;
+
+/**
+ * @ClassName: MyEighthThreadA
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author Songzx songzx_2326@163.com
+ * @date 2017年10月17日 下午9:34:48
+ * 
+ */
+public class MyEighthThreadB extends Thread {
+	/**
+	 * 类Exchanger的功能可以使两个线程之间传输数据
+	 */
+	private Exchanger<String> exchanger;
+
+	/**
+	 * @Date: 2017年10月17日下午9:35:40
+	 * @Title: MyEighthThreadA.java
+	 * @Description: TODO(这里用一句话描述这个方法的作用)
+	 * @param exchanger
+	 */
+	public MyEighthThreadB(Exchanger<String> exchanger) {
+		super();
+		this.exchanger = exchanger;
+	}
+
+	public void run() {
+		try {
+			/*
+			 * 类Exchanger中的exchanger()方法具有阻塞特性，也就是此方法被调用后等待其他线程来取得数据，如果没有则一直阻塞等待
+			 */
+			System.out.println("在线程B中得到线程A的值：" + exchanger.exchange("中国人B！"));
+			System.out.println("B end！");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/cn/songzx/semaph/eighth/test/MyEighthRunA.java
@@ -0,0 +1,38 @@
+/**   
+* @Title: MyEighthRunA.java 
+* @Package cn.songzx.semaph.eighth.test 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author Songzx songzx_2326@163.com   
+* @date 2017年10月17日 下午9:38:12 
+* @version V1.0   
+*/
+package cn.songzx.semaph.eighth.test;
+
+import java.util.concurrent.Exchanger;
+
+import cn.songzx.semaph.eighth.extthread.MyEighthThreadA;
+
+/**
+ * @ClassName: MyEighthRunA
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author Songzx songzx_2326@163.com
+ * @date 2017年10月17日 下午9:38:12
+ * 
+ */
+public class MyEighthRunA {
+
+	/**
+	 * @Date: 2017年10月17日下午9:38:12
+	 * @Title: main
+	 * @Description: TODO(这里用一句话描述这个方法的作用)
+	 * @param args
+	 * @return void 返回值类型
+	 */
+	public static void main(String[] args) {
+		Exchanger<String> exchanger = new Exchanger<String>();
+		MyEighthThreadA a = new MyEighthThreadA(exchanger);
+		a.start();
+		System.out.println("main end！");
+	}
+
+}
